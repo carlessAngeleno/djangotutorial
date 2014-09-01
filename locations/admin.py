097d0b846bd30c204e7cfa82fee85a8914fbb795
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 19:07:27 -0700

    Break up fields into fieldsets

--- locations/admin.py
+++ locations/admin.py
@@ -3,15 +3,20 @@ from locations.models import Vehicle
 
 # Register your models here.
 class VehicleAdmin(admin.ModelAdmin):
-    fields = [
-        'run_id', 
-        'vehicle_id', 
-        'route_id', 
-        'heading',
-        'seconds_since_report',
-        'latitude',
-        'longitude',
-        'predictable'
+    fieldsets = [
+        ('Vehicle', {'fields': ['run_id', 'vehicle_id', 'route_id']}),
+        (
+            'Status',  
+            {
+                'fields': [
+                    'seconds_since_report', 
+                    'latitude', 
+                    'longitude',
+                    'heading'
+                ]
+            }
+        ),
+        ('Other', {'fields': ['predictable']})
     ]
 
 admin.site.register(Vehicle, VehicleAdmin)
