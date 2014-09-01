8703802d828586a6cb65d8905c9b458b670a0e84
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 19:01:26 -0700

    Customize admin form for Vehicle

--- locations/admin.py
+++ locations/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from locations.models import Vehicle
 
 # Register your models here.
-admin.site.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    fields = [
+        'run_id', 
+        'vehicle_id', 
+        'route_id', 
+        'heading',
+        'seconds_since_report',
+        'latitude',
+        'longitude',
+        'predictable'
+    ]
+
+admin.site.register(Vehicle, VehicleAdmin)
