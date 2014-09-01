8511b64df2f87cc3627da5c7b57d161f938744a3
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 18:25:28 -0700

    Add new model, vehicle

--- angelesbus/models.py
+++ angelesbus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Vehicle(models.Model):
+    seconds_since_report = models.IntegerField()
+    run_id = models.CharField()
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    heading = models.DecimalField(max_digits=4, decimal_places=1)
+    route_id = models.CharField()
+    predictable = models.BooleanField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    id = models.CharField()
