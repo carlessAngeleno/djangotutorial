638c983516e9e9e6fc26072a85037f78df9e6575
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 21:05:52 -0700

    Use a template

--- locations/views.py
+++ locations/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import RequestContext, loader
 
 from locations.models import Vehicle
 
 # Create your views here.
 def index(request):
     latest_vehicle_list = Vehicle.objects.order_by('-seconds_since_report')[:5]
-    output = ', '.join([v.vehicle_id for v in latest_vehicle_list])
-    return HttpResponse(output)
+    template = loader.get_template('locations/index.html')
+    context = RequestContext(request, {
+        'latest_vehicle_list': latest_vehicle_list,
+    })
+    return HttpResponse(template.render(context))
+
 
 def detail(request, vehicle_id):
     return HttpResponse("You're looking at vehicle %s." % vehicle_id)
 
+
 def results(request, vehicle_id):
     return HttpResponse("You're looking at the results of poll %s." % vehicle_id)
+
 
 def vote(request, vehicle_id):
     return HttpResponse("You're voting on poll %s." % vehicle_id)
