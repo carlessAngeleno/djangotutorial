4dbd769ade6f07aa7e52aaa737f18b7f80640565
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 18:28:38 -0700

    Bugfix: rename id field + add max_length to charfields

--- angelesbus/models.py
+++ angelesbus/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Vehicle(models.Model):
     seconds_since_report = models.IntegerField()
-    run_id = models.CharField()
+    run_id = models.CharField(max_length=50)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     heading = models.DecimalField(max_digits=4, decimal_places=1)
-    route_id = models.CharField()
+    route_id = models.CharField(max_length=50)
     predictable = models.BooleanField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    id = models.CharField()
+    vehicle_id = models.CharField(max_length=50)
