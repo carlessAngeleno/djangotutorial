8f4570880ab9fb65bdfc9c7eeab31c52f2ba5fd2
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 21:22:50 -0700

    Use a shortcut to render

--- locations/views.py
+++ locations/views.py
@@ -1,17 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import RequestContext, loader
 
 from locations.models import Vehicle
 
 # Create your views here.
 def index(request):
     latest_vehicle_list = Vehicle.objects.order_by('-seconds_since_report')[:5]
-    template = loader.get_template('locations/index.html')
-    context = RequestContext(request, {
-        'latest_vehicle_list': latest_vehicle_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_vehicle_list': latest_vehicle_list}
+    return render(request, 'locations/index.html', context)
 
 
 def detail(request, vehicle_id):
