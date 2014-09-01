cc5dded8bc1af5af76e3ccc7fb4a93e564b1b5bb
carlessAngeleno <daydream.han@gmail.com>
2014-08-31 20:47:20 -0700

    Add more views

--- locations/views.py
+++ locations/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
 
+def detail(request, vehicle_id):
+    return HttpResponse("You're looking at vehicle %s." % vehicle_id)
+
+def results(request, vehicle_id):
+    return HttpResponse("You're looking at the results of poll %s." % vehicle_id)
+
+def vote(request, vehicle_id):
+    return HttpResponse("You're voting on poll %s." % vehicle_id)
