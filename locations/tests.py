210d14c3e0e3c9c03f4c21ef803e451096c44ca0
carlessAngeleno <daydream.han@gmail.com>
2014-09-01 21:21:32 -0700

    Write more tests

--- locations/tests.py
+++ locations/tests.py
@@ -20,3 +20,37 @@ class VehicleMethodTests(TestCase):
             predictable=True
         )
         self.assertEqual(future_report.was_reported_recently(), False)
+
+    def test_was_reported_recently_with_old_report(self):
+        """
+        was_reported_recently() should return False for vehicles whose
+        seconds_since_report is older than 30 seconds
+        """
+        future_report = Vehicle(
+            run_id='201_38_01',
+            vehicle_id='7184',
+            route_id='201',
+            seconds_since_report=31,
+            latitude=34.07349,
+            longitude=-118.288185,
+            heading=255,
+            predictable=True
+        )
+        self.assertEqual(future_report.was_reported_recently(), False)
+
+    def test_was_reported_recently_with_recent_report(self):
+        """
+        was_reported_recently() should return False for vehicles whose
+        seconds_since_report is less than or equal to 30
+        """
+        future_report = Vehicle(
+            run_id='201_38_01',
+            vehicle_id='7184',
+            route_id='201',
+            seconds_since_report=29,
+            latitude=34.07349,
+            longitude=-118.288185,
+            heading=255,
+            predictable=True
+        )
+        self.assertEqual(future_report.was_reported_recently(), True)        
