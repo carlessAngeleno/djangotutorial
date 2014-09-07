39f3272c666eb46e8e074c4e4556e52b8252a10a
carlessAngeleno <daydream.han@gmail.com>
2014-09-06 18:22:16 -0700

    Customize admin look

--- locations/admin.py
+++ locations/admin.py
@@ -24,5 +24,18 @@ class VehicleAdmin(admin.ModelAdmin):
     list_filter = ['vehicle_id', 'route_id']
     search_fields = ['vehicle_id']
 
+
+class StopAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Stop', {'fields': ['stop_id', 'display_name']}),
+        ('Location', {'fields': ['latitude', 'longitude']})
+    ]
+
+    list_display = ('stop_id', 'display_name', 'latitude', 'longitude')
+
+    list_filter = ['latitude', 'longitude']
+    search_fields = ['display_name']
+
+
 admin.site.register(Vehicle, VehicleAdmin)
-admin.site.register(Stop)
+admin.site.register(Stop, StopAdmin)
