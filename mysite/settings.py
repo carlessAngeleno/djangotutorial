419346a4f1735bc2ad575b31e98431e4a9e94563
carlessAngeleno <daydream.han@gmail.com>
2015-04-11 15:20:03 -0700

    Doodle

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
